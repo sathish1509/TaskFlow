afdafc023c17790f01a8e341293b5a3391ab69cc
sathish1509 <sathish.p2024cse@sece.ac.in>
2026-04-06 10:50:19 +0530

    client side added

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-start bg-gray-900">
+      <h1 className="mt-12 text-5xl font-bold text-blue-500 text-center">
+        TASKFLOW
+      </h1>
+    </div>
+  );
+}
+
+export default App;
